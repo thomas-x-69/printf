dfb3e90d751d0c6e1d2d272f350bb5901ee7432c
thomas-x-69 <thomas.ashraf975@gmail.com>
2023-11-14 04:38:41 +0200

    new

--- specifier.c
+++ specifier.c
@@ -10,19 +10,9 @@ int (*get_specifier(char *s))(va_list ap, params_t *params)
 {
 	specifier_t specifiers[] = {
 		{"c", print_char},
-		{"d", print_int},
-		{"i", print_int},
 		{"s", print_string},
 		{"%", print_percent},
-		{"b", print_binary},
-		{"o", print_octal},
-		{"u", print_unsigned},
-		{"x", print_hex},
-		{"X", print_HEX},
-		{"p", print_address},
-		{"S", print_S},
-		{"r", print_rev},
-		{"R", print_rot13},
+	
 		{NULL, NULL}
 	};
 	int i = 0;
