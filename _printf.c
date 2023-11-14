11890e163f96e0ca2a623bed073b5b4e506732d6
thomas-x-69 <thomas.ashraf975@gmail.com>
2023-11-14 04:11:01 +0200

    Some adjustments

--- _printf.c
+++ _printf.c
@@ -29,18 +29,18 @@ int _printf(const char *format, ...)
 		}
 		start = p;
 		p++;
-		while (get_flag(p, &params)) /* while char at p is flag char */
-		{
-			p++; /* next char */
-		}
-		p = get_width(p, &params, ap);
-		p = get_precision(p, &params, ap);
+		// while (get_flag(p, &params)) /* while char at p is flag char */
+		// {
+		// 	p++; /* next char */
+		// }
+		// p = get_width(p, &params, ap);
+		// p = get_precision(p, &params, ap);
 		if (get_modifier(p, &params))
 			p++;
-		if (!get_specifier(p))
-			sum += print_from_to(start, p,
-				params.l_modifier || params.h_modifier ? p - 1 : 0);
-		else
+		// if (!get_specifier(p))
+		// 	sum += print_from_to(start, p,
+		// 		params.l_modifier || params.h_modifier ? p - 1 : 0);
+		// else
 			sum += get_print_func(p, ap, &params);
 	}
 	_putchar(BUF_FLUSH);
